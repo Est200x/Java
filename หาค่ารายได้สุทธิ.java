42e5f24de394b7c4c1a56be21c343bd64b2dea59
Wuttiphat Munphathi <Wuttiphatmulphathi@gmail.com>
2025-09-03 19:54:07 +0700

    Update หาค่ารายได้สุทธิ.java

--- หาค่ารายได้สุทธิ.java
+++ หาค่ารายได้สุทธิ.java
@@ -4,28 +4,24 @@ public class CommissionCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       
         System.out.print("ป้อนยอดขาย: ");
         double sales = sc.nextDouble();
 
-        double commission = 0;
+        double commission;
 
-      
-        if (sales >= 1 && sales <= 5000) {
-            commission = sales * 0.05;
-        } else if (sales >= 5001 && sales <= 10000) {
-            commission = sales * 0.10;
-        } else if (sales >= 10001 && sales <= 20000) {
-            commission = sales * 0.15;
-        } else if (sales > 20000) {
-            commission = sales * 0.20;
-        } else {
+        if (sales <= 0) {
             commission = 0;
+        } else if (sales <= 5000) {
+            commission = sales * 0.05;
+        } else if (sales <= 10000) {
+            commission = sales * 0.10;
+        } else if (sales <= 20000) {
+            commission = sales * 0.15;
+        } else {
+            commission = sales * 0.20;
         }
 
-        
         System.out.println("ยอดขาย: " + sales + " บาท");
         System.out.println("ค่าคอมมิชชั่น: " + commission + " บาท");
     }
 }
-
